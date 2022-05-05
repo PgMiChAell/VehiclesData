9ca15459efc04d763bf3c5f189bacc651e5f6ddd
PgMiChAell <95360577+PgMiChAell@users.noreply.github.com>
2022-05-05 10:27:14 -0400

    Initial commit

---
+++ src/com/company/BestNCS.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class BestNCS {
+    //fields
+    int Country;
+    int Image;
+    String YearsSold;
+    int AmountSold;
+    String Automobile;
+
+    //methods
+    void describe(){
+        System.out.println("The Car sales " + Automobile + ", numbers " + AmountSold + " Years Sold " + YearsSold  );
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("I hope you like cars");
+        BestNCS Car1 = new BestNCS();
+                Car1.Country = 1;
+                Car1.YearsSold = "1960-2016";
+                Car1.AmountSold = 3578689;
+                Car1.Automobile = " Ford Falcon ";
+
+                Car1.describe();
+    }
+}
