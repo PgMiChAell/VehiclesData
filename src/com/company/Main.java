342fd6b94c043251c97ed69e3feb83fc38b67cd1
PgMiChAell <95360577+PgMiChAell@users.noreply.github.com>
2022-05-11 08:46:46 -0400

    Updated  5/11/2022

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,8 +8,9 @@ public class Main {
         BestNCS Car1 = new BestNCS();
                 Car1.Country = 1;
                 Car1.YearsSold = "1960-2016";
-                Car1.AmountSold = 3578689;
-                Car1.Automobile = " Ford Falcon ";
+                Car1 = 3578689;
+                Car1 = " Ford Falcon ";
+                // michael is  here
 
                 Car1.describe();
 
